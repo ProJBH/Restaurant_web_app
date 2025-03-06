40a15bd2faf4c889afbb98d6f5ff79fa2f3e1020
ProJBH <nzbohuajia@gmail.com>
2025-03-06 08:42:56 +0100

    use DiscountCarousel component instead of Discount and set some temp data

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,9 +3,38 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import Discount from "../components/Discount";
+import DiscountCarousel from "../components/DiscountCarousel/DiscountCarousel";
+import springRoll from "../assets/springrolls.jpg";
+import dongpoPork from "../assets/dongpopork.jpg";
 
 const HomePage: React.FC = () => {
+  const carouselItems = [
+    {
+      id: 1,
+      image: springRoll,
+      discount: '50% OFF',
+      title: 'Spring Rolls',
+      originalPrice: 98,
+      discountedPrice: 49,
+      timeLimit: '12:34:56'
+    },
+    {
+      id: 2,
+      image: dongpoPork,
+      discount: '30% OFF',
+      title: 'Dongpo Pork',
+      originalPrice: 88,
+      discountedPrice: 61,
+    },
+    {
+      id: 3,
+      image: 'discount3.jpg',
+      discount: '40% OFF',
+      title: 'Dish 3',
+      originalPrice: 108,
+      discountedPrice: 65,
+    },
+  ];
   return (
     <div>
       <header>
@@ -13,7 +42,9 @@ const HomePage: React.FC = () => {
         <h1>Welcome to Our Restaurant</h1>
       </header>
       <section>
-        <Discount />
+        <DiscountCarousel 
+        items={carouselItems}
+        autoPlayInterval={5000}/>
       </section>
       <section>
         <h2>Advertisement</h2>
