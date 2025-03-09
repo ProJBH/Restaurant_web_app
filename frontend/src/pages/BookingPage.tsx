4f7878f9072b26bf1c051c9dd4130614cf92945c
ProJBH <nzbohuajia@gmail.com>
2025-03-09 20:25:42 +0100

    remove old version of booking form and use booking component instead

--- frontend/src/pages/BookingPage.tsx
+++ frontend/src/pages/BookingPage.tsx
@@ -1,22 +1,11 @@
 // src/pages/BookingPage.tsx
-import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Navbar from "../components/Navbar/Navbar";
+import Booking from '../components/Booking/Booking';
+import styles from "./BookingPage.module.scss"; 
 
 const BookingPage: React.FC = () => {
-  const [form, setForm] = useState({ name: '', email: '', date: '', time: '', guests: 1 });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // For now, simply log the data. Later, connect to a backend endpoint.
-    console.log(form);
-  };
-
   return (
     <div>
       <div>
@@ -24,15 +13,9 @@ const BookingPage: React.FC = () => {
           <Navbar />
         </header>
       </div>
-      <h1>Book a Table</h1>
-      <form onSubmit={handleSubmit}>
-        <input name="name" placeholder="Your Name" value={form.name} onChange={handleChange} required />
-        <input name="email" type="email" placeholder="Email" value={form.email} onChange={handleChange} required />
-        <input name="date" type="date" value={form.date} onChange={handleChange} required />
-        <input name="time" type="time" value={form.time} onChange={handleChange} required />
-        <input name="guests" type="number" min="1" value={form.guests} onChange={handleChange} required />
-        <button type="submit">Submit</button>
-      </form>
+      <div className={styles.BookingPageContainer}>
+        <Booking />
+      </div>
     </div>
   );
 };
