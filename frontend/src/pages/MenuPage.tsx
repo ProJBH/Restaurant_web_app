41bb6497233d43480a045f77d76df25d27323478
ProJBH <nzbohuajia@gmail.com>
2025-03-09 11:04:49 +0100

    add background img

--- frontend/src/pages/MenuPage.tsx
+++ frontend/src/pages/MenuPage.tsx
@@ -1,14 +1,16 @@
 // src/pages/MenuPage.tsx
+import React from "react";
 import Navbar from "../components/Navbar";
 import Menu from "../components/Menu/Menu";
+import styles from "./MenuPage.module.scss"; // 引入 CSS 模块
 
 const MenuPage: React.FC = () => {
   return (
-    <div>
+    <div className={styles.menuPageContainer}>
       <Navbar />
       <Menu />
     </div>
   );
-}
+};
 
 export default MenuPage;
