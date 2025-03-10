f16e7fd2185cfd84bfa658412af0f14a67c23316
ProJBH <nzbohuajia@gmail.com>
2025-03-10 09:39:03 +0100

    change path from /admin/login to /login better fit for further development

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BookingPage from './pages/BookingPage';
 import MenuPage from './pages/MenuPage';
-import AdminLogin from './pages/AdminLogin';
+import AdminLogin from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
 import AboutUsPage from './pages/AboutUsPage';
 
@@ -16,7 +16,7 @@ const App: React.FC = () => {
         <Route path="/booking" element={<BookingPage />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path='/aboutus' element={<AboutUsPage />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/login" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
     </Router>
